9684ca52a6c590e8cac8a447cc82365b99341c8a
pochiman <pochiman@yahoo.com>
2023-07-27 18:24:38 -0400

    initial commit

---
+++ 002_Variables_and_Simple_Data_Types/009_stripping_whitespace.py
@@ -0,0 +1,20 @@
+'''
+
+>>> favorite_language = 'python '
+>>> favorite_language
+'python '
+
+>>> favorite_language = 'python '
+>>> favorite_language = favorite_language.rstrip()
+>>> favorite_language
+'python'
+
+>>> favorite_language = ' python '
+>>> favorite_language.rstrip()
+' python'
+>>> favorite_language.lstrip()
+'python '
+>>> favorite_language.strip()
+'python'
+
+'''
---
+++ 002_Variables_and_Simple_Data_Types/010_removing_prefixes.py
@@ -0,0 +1,9 @@
+'''
+
+>>> nostarch_url = 'https://nostarch.com'
+>>> nostarch_url.removeprefix('https://')
+'nostarch.com'
+
+>>> simple_url = nostarch_url.removeprefix('https://')
+
+'''
