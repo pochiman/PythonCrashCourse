616496288a1e8e395109c25d70a22f534ee9532b
pochiman <pochiman@yahoo.com>
2023-08-03 22:34:44 -0400

    initial commit

---
+++ 008_Functions/try_8-15_printing_models.py
@@ -0,0 +1,7 @@
+import try_8-15-1_printing_functions as pf
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
